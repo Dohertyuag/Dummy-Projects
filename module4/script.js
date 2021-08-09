70d810ca702babf90f0bc3fd7556aae714c9398e
Amey Vasant Kumbhare <67731264+Dohertyuag@users.noreply.github.com>
2021-08-09 12:25:13 +0530

    Editted range of for loop

--- module4/script.js
+++ module4/script.js
@@ -1,6 +1,6 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (i = 0; i <= names.length; i++) {
+for (i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();   //Taking the i^th name, lower-casing it's
                                                         //first letter and asigning it to firstLetter
   if (firstLetter == 'j') {                             // or if (firstLetter === j)
